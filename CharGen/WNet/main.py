8e751baf47e458b2e29b61acc5d01d1203295969
Zhenduo Wang <wzdlc1996@gmail.com>
2022-03-29 15:46:20 +0800

    begin WNet revisiting

---
+++ CharGen/WNet/main.py
@@ -0,0 +1,22 @@
+"""
+Is the pytorch implement for paper
+
+One-shot Arbitrary-style Chinese Character Generation
+
+It is the basic code implementation of the paper W-Net: One-shot Arbitrary-style Chinese Character Generation in the
+ 24th International Conference on Neural Information Processing (ICONIP-2018), authored by Haochuan JIANG, Guanyu YANG,
+  Kaizhu HUANG and Rui ZHANG from the Department of Electrical and Electronic Engineering, Xi'an Jiaotong-Liverpool
+   University, SIP, Suzhou, P.R.China.
+
+The authors' repo is
+    https://github.com/falconjhc/W-Net/
+
+References:
+    https://github.com/Arctanxy/W-Net-PyTorch
+"""
+import torch
+
+
+if __name__ == "__main__":
+    print("hello world!")
+    print(torch.cuda.is_available())
